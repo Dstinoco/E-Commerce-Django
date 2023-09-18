7b13341b99a7fbc2d98c2ae816e749dfec43d317
Douglas Tinoco <douglas.bart@hotmail.com>
2023-09-18 13:01:16 -0300

    Exbindo dados do banco na página

--- djangoecormerce/urls.py
+++ djangoecormerce/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index, product, product_list, contact
+from catalog import views as views_catalog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('produto/', product, name='produto'),
-    path('lista_produto/', product_list, name='lista_produto'),
     path('contato/', contact, name='contato'),
+    path('produtos/', include('catalog.urls', namespace="catalog-")),
 ]
